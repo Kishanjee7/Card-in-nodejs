5aeace710de5433bc67ff844687e600d1558c9e7
Kishanjee kumar <85598008+Kishanjee7@users.noreply.github.com>
2025-01-29 18:18:03 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/Cards.jsx"
+
+function App() {
+  
+  return (
+    <>
+    <div className="flex justify-center ">
+    <Card />
+    </div>
+      
+    </>
+  )
+}
+
+export default App
